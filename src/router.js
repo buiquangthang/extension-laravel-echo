79f485c1304acd5208e95df4b5a51cfe33a41ff2
ThangBQ <buiquangthangit@gmail.com>
2020-11-02 23:12:34 +0700

    Feature/ Authentication

--- src/router.js
+++ src/router.js
@@ -32,4 +32,17 @@ export default new Router({
       ]
     }
   ]
-})
+});
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
+});
